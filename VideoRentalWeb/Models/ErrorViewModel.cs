75849f688cff022018c4785dcd58674e92695021
bende-r <br242424@mail.ru>
2024-01-10 18:58:32 +0300

    commit

--- VideoRentalWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace VideoRentalWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
